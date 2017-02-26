0a504e274d7befce224d553cf870acfacc752873
Peter Swantek <swantek2@gmail.com>
2017-02-25 22:18:06 -0600

    create bootstrap.sh file and update the Vagrant file

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sudo yum -y update
+sudo yum -y install wget
+sudo yum -y install dos2unix
+
+sudo wget --no-cookies --no-check-certificate --header  "Cookie: oraclelicense=accept-securebackup-cookie" \ 
+"http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.rpm" -O /tmp/jdk-8-linux-x64.rpm
+
+sudo yum -y install /tmp/jdk-8-linux-x64.rpm
+sudo rm /tmp/jdk-8-linux-x64.rpm
